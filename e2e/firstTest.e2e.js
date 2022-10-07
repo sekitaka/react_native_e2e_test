2221a1bf51e048869a66b5b3b0bb6592fa1a729b
sekitaka <sekitaka.tech@gmail.com>
2022-10-07 15:57:25 +0900

    Add actions.

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -2,13 +2,21 @@
 describe('Example', () => {
   beforeAll(async () => {
     await device.launchApp();
+    const imagePath = await device.takeScreenshot('after launch');
   });
 
   beforeEach(async () => {
     await device.reloadReactNative();
   });
 
+  // it('should show "Ready?"', async () => {
+  //   const imagePath = await device.takeScreenshot('opened general section');
+  //   await expect(element(by.id('textId'))).toBeVisible();
+  //   await expect(element(by.id('textId'))).toHaveText('Ready?');
+  // });
   it('should show "Ready?"', async () => {
+    // await expect("a").toEqual("a");
+    const imagePath = await device.takeScreenshot('opened general section');
     await expect(element(by.id('textId'))).toBeVisible();
     await expect(element(by.id('textId'))).toHaveText('Ready?');
   });
